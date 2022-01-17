2ef2da77882edeb9a6d61b9ca644421b61fb4277
ushliypakostnik <levon.gambaryan@gmail.com>
2022-01-17 23:43:59 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>Test content of test poject!!!</Layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import ComponentLibrary from 'ui-library-starter-2';
+
+import store, { key } from 'ui-library-starter-2/src/store/index';
+
+createApp(App).use(ComponentLibrary).use(store, key).mount('#app');
